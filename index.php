05a822adbb439435be50e19c8e3676a0aa735d1b
MildThrone83943 <aaron2.0702@outlook.com>
2020-12-03 09:25:32 +0000

    random error message page

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="HandheldFriendly" content="true">
+
+        <title>
+            Page Not Found
+        </title>
+
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+        <div>
+            <span>
+                Something Happened
+            </span><br><hr>
+            <article>
+                Oops...check your internet connection and try again...
+            </article>
+        </div>
+    </body>
+</html>
