b34c0ef8e0d4ebae3ea96b158e8304bc1da48fef
MildThrone83943 <aaron2.0702@outlook.com>
2021-07-30 09:49:48 +0000

    redesigned the page

--- index.php
+++ index.php
@@ -12,16 +12,33 @@
         </title>
 
         <link rel="stylesheet" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Style+Script&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
 
     <body>
-        <div>
-            <span>
-                Something Happened
-            </span><br><hr>
-            <article>
-                Oops...check your internet connection and try again...
-            </article>
+        <div class="error">
+            <div class="error-code">
+                <span class="four">4</span>
+                <span class="zero">0</span>
+                <span class="four">4</span>
+            </div>
+
+            <div class="error-message">
+                <span>The page you're looking for is missing</span>
+                <br><br>
+                <div class="search">
+                    <form action="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                        <input placeholder="Try searching for something">
+                        <button onclick="location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     </body>
 </html>
