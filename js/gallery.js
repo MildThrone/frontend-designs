06d7412be18fc8a86235dfc6132a3d29e5d1069d
MildThrone83943 <aaron2.0702@outlook.com>
2021-09-06 11:01:30 +0000

    update for the gallery bit

---
+++ js/gallery.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('img.image').forEach((item) => {
+    item.addEventListener('click', (event) => {
+        let image = event.target.getAttribute('src');
+        document.getElementById('modal').style
+    })
+});
